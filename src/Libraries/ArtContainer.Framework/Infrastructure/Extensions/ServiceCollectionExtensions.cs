48b25bd624081fcd7811e0897b4bf3b42874269e
JoeyTribbiani1995 <vophanhongdung@gmail.com>
2020-01-22 21:44:48 +0700

    add extension for startup file

---
+++ src/Libraries/ArtContainer.Framework/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ArtContainer.Framework.Infrastructure.Extensions
+{
+    /// <summary>
+    /// Represents extensions of IServiceCollection
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceProvider ConfigureApplicationServices(this IServiceCollection services,
+     IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+
+            return serviceProvider;
+        }
+    }
+}
